21810510b225821e36c57f8265eb2935ae8648fa
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2009-04-08 14:52:06 +0000

    [OPENBUS-64] Adicionando um assert para caso o usuário não consiga desempacotar.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk/tools@90256 ae0415b3-e90b-0410-900d-d0be9363c56b

--- lua/tools/util.lua
+++ lua/tools/util.lua
@@ -84,7 +84,7 @@ function fetch_and_unpack(package,from,to)
 		local fetch_cmd = "curl -o ".. package ..".tar.gz ".. from .." || wget ".. from
 		assert(os.execute("cd ".. PRODAPP .."; ".. fetch_cmd) == 0, "ERROR: Unable to download the package '"..package.."' using 'curl' neither 'wget' commands. You must download this package manually from '"..from.."' and extract it in the '"..PRODAPP.."' directory.")
 		local unpack_cmd = "gzip -c -d ".. package ..".tar.gz |tar -xf -"
-		os.execute("cd ".. PRODAPP .."; ".. unpack_cmd)
+		assert(os.execute("cd ".. PRODAPP .."; ".. unpack_cmd) ==0, "ERROR: Unable to extract the package '".. package.."' in the directory '"..PRODAPP.."'.")
 	end
 end
 
