9a245e5712ece83f7d415ab83683153932d00520
Amadeu Andrade Babosa Junior <amadeu@tecgraf.puc-rio.br>
2009-09-09 16:49:41 +0000

    [ OPENBUS-361 , OPENBUS-362 ] BugFix: inclusão do descritor de pacote para o SCS.
    Melhoria: implementação do suporte ao ANT e integração com
    a instalação no repositório local do MAVEN.
    Criei um pacote "scs-java2maven" para instalar os artefatos 
    compilados através do ANT no repositório local do MAVEN.
    
    Correção nos arquivos pom.xml para descrever os artefatos dos
    quais o JacORB depende, para que assim seja possível compilar
    os pacotes Java do Openbus através do tools e sem obrigar uso
    de uma IDE (como Eclipse).
    
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk/tools@95969 ae0415b3-e90b-0410-900d-d0be9363c56b

--- lua/tools/build/maven.lua
+++ lua/tools/build/maven.lua
@@ -17,23 +17,39 @@ function run(t, arguments)
 
 	-- Making command
 	local maven_cmd =  "mvn "
-	if arguments["rebuild"] then
-		maven_cmd = maven_cmd .. "clean "
+	if not t.build.legacymode then
+		if arguments["rebuild"] then
+			maven_cmd = maven_cmd .. "clean "
+		end
+		maven_cmd = maven_cmd .. "install "
+
+		-- Adding arguments
+		local maven_args = " -DskipTests " 
+		if not arguments["verbose"] and not arguments["v"] then 
+			maven_args = maven_args .. "-q " 
+		end
+
+		build_cmd = "cd " .. build_dir .. " && " .. maven_cmd .. maven_args
+
+		local ret = os.execute(build_cmd)
+		-- assert ensure that we could continue
+		assert(ret == 0,"ERROR compiling the software ".. t.name .."")
 	end
-	maven_cmd = maven_cmd .. "install "
-
-	-- Adding arguments
-	local maven_args = " -DskipTests " 
-	if not arguments["verbose"] and not arguments["v"] then 
-		maven_args = maven_args .. "-q " 
+	-- Triggers after the compilation
+	if t.maven_post_compile then
+		assert(type(t.maven_post_compile)=="table")
+		for file, props in pairs(t.maven_post_compile) do
+			local repoinstall = " install:install-file -DskipTests -DgroupId=" .. props.groupId ..
+					" -DartifactId=".. props.artifactId ..
+					" -Dversion=" .. props.version .. 
+					" -Dpackaging=jar -Dfile=" .. file
+			local ret = os.execute("cd ".. build_dir .." && ".. maven_cmd .. repoinstall)
+			-- assert ensure that we could continue
+			assert(ret == 0,"ERROR compiling the software ".. t.name ..
+				" when it tried to install the file: ".. file .. 
+				" in the maven repository.")
+		end
 	end
-
-	build_cmd = "cd " .. build_dir .. " && " .. maven_cmd .. maven_args
-
-	local ret = os.execute(build_cmd)
-	-- assert ensure that we could continue
-	assert(ret == 0,"ERROR compiling the software ".. t.name .."")
-
 	-- re-using copy method to parse install_files, conf_files, dev_files
 	copy.run(t,arguments,build_dir)
 end
