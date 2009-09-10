09a34c0e7b1f88be43fc9b5c9c17baf61ada6890
Mauricio Arieira Rosas <marosas@tecgraf.puc-rio.br>
2009-09-10 17:03:51 +0000

    [OPENBUS-366] O tools está tentando executar o "svn update" onde deveria executar "svn checkout"
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk/tools@96032 ae0415b3-e90b-0410-900d-d0be9363c56b

--- lua/tools/fetch/svn.lua
+++ lua/tools/fetch/svn.lua
@@ -8,7 +8,9 @@ function run(path, url)
 	assert(path and url)
 	local download_cmd
 	if os.execute("which svn >/dev/null 2>/dev/null") == 0 then
-		if os.execute("svn info ".. path .." >/dev/null 2>/dev/null") == 0 then
+		if os.execute("svn info ".. path .." >/dev/null 2>/dev/null") == 0 and
+                   -- Versões anteriores ao svn 1.6 não retornam erros quando o path não existe.
+                   os.execute("test -d " .. path) == 0 then
 			download_cmd = "svn up ".. path
 			-- allowing the svn up returns errors like 'old working copy'
 			if os.execute(download_cmd) ~= 0 then
