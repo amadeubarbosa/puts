ba11d1d97457896b29c72f4f3adf5df35cd28dc2
Carlos Eduardo Lara Augusto <augusto@tecgraf.puc-rio.br>
2009-10-01 23:24:30 +0000

    Corrigindo formatação dos arquivos maven para o tools.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/trunk/tools@97008 ae0415b3-e90b-0410-900d-d0be9363c56b

--- lua/tools/build/maven.lua
+++ lua/tools/build/maven.lua
@@ -11,46 +11,46 @@ local myplat = platforms[TEC_SYSNAME]
 module("tools.build.maven", package.seeall)
 
 function run(t, arguments)
-	print("[ INFO ] Compiling package via maven: ".. t.name)
-	local build_dir = t.build.src
-	if not build_dir:match("/$") then build_dir = build_dir.."/" end
+  print("[ INFO ] Compiling package via maven: ".. t.name)
+  local build_dir = t.build.src
+  if not build_dir:match("/$") then build_dir = build_dir.."/" end
 
-	-- Making command
-	local maven_cmd =  "mvn "
-	if arguments["rebuild"] then
-		maven_cmd = maven_cmd .. "clean "
-	end
-	maven_cmd = maven_cmd .. "install "
+  -- Making command
+  local maven_cmd =  "mvn "
+  if arguments["rebuild"] then
+    maven_cmd = maven_cmd .. "clean "
+  end
+  maven_cmd = maven_cmd .. "install "
 
-	-- Adding arguments
-	local maven_args = " -DskipTests " 
-	if not arguments["verbose"] and not arguments["v"] then 
-		maven_args = maven_args .. "-q " 
-	end
+  -- Adding arguments
+  local maven_args = " -DskipTests " 
+  if not arguments["verbose"] and not arguments["v"] then 
+    maven_args = maven_args .. "-q " 
+  end
 
-	build_cmd = "cd " .. build_dir .. " && " .. maven_cmd .. maven_args
+  build_cmd = "cd " .. build_dir .. " && " .. maven_cmd .. maven_args
 
-	local ret = os.execute(build_cmd)
-	-- assert ensure that we could continue
-	assert(ret == 0,"ERROR compiling the software ".. t.name .."")
+  local ret = os.execute(build_cmd)
+  -- assert ensure that we could continue
+  assert(ret == 0,"ERROR compiling the software ".. t.name .."")
 
-	-- re-using copy method to parse install_files, conf_files, dev_files
-	copyDependence(t,arguments,build_dir)
-	copy.run(t,arguments,build_dir)
+  -- re-using copy method to parse install_files, conf_files, dev_files
+  copyDependence(t,arguments,build_dir)
+  copy.run(t,arguments,build_dir)
 end
 
 function copyDependence(t,arguments,build_dir)
-	local maven_cmd = "mvn "
-        maven_cmd = maven_cmd .. "dependency:copy-dependencies "
+  local maven_cmd = "mvn "
+  maven_cmd = maven_cmd .. "dependency:copy-dependencies "
+  maven_args = "-DincludeScope=runtime"
 
-		maven_args = "-DincludeScope=runtime"
-        -- Adding arguments
-        if not arguments["verbose"] and not arguments["v"] then
-                maven_args = maven_args .. " -Dsilent=true "
-        end
+  -- Adding arguments
+  if not arguments["verbose"] and not arguments["v"] then
+    maven_args = maven_args .. " -Dsilent=true "
+  end
 	
-	build_cmd = "cd " .. build_dir .. " && " .. maven_cmd .. maven_args
+  build_cmd = "cd " .. build_dir .. " && " .. maven_cmd .. maven_args
 
-	local ret = os.execute(build_cmd)
-	assert(ret == 0, "ERRO copyng-dependencies" .. t.name)
+  local ret = os.execute(build_cmd)
+  assert(ret == 0, "ERRO copying-dependencies" .. t.name)
 end 
--- lua/tools/build/mavenimport.lua
+++ lua/tools/build/mavenimport.lua
@@ -12,38 +12,39 @@ local myplat = platforms[TEC_SYSNAME]
 module("tools.build.mavenimport", package.seeall)
 
 function run(t, arguments)
-	print("[ INFO ] Importing package via maven: ".. t.name)
-	local build_dir = t.build.src
-        if not build_dir:match("/$") then build_dir = build_dir.."/" end
+  print("[ INFO ] Importing package via maven: ".. t.name)
+  local build_dir = t.build.src
+  if not build_dir:match("/$") then build_dir = build_dir.."/" end
 
-	-- Making command
-	local maven_cmd =  "mvn "
-	maven_cmd = maven_cmd .. "install:install-file "
+  -- Making command
+  local maven_cmd =  "mvn "
+  maven_cmd = maven_cmd .. "install:install-file "
 
-	-- Adding arguments
-	local maven_args = "-DskipTests " 
-	if not arguments["verbose"] and not arguments["v"] then 
-		maven_args = maven_args .. "-q " 
-	end
+  -- Adding arguments
+  local maven_args = "-DskipTests " 
+  if not arguments["verbose"] and not arguments["v"] then 
+    maven_args = maven_args .. "-q " 
+  end
 
-	assert(type(t.parameters)=="table")
-	for file, props in pairs(t.parameters) do
-		local repoinstall =
-				" -DgroupId="    .. props.groupId ..
-				" -DartifactId=" .. props.artifactId ..
-				" -Dversion="    .. props.version .. 
-				" -Dpackaging="  .. "jar" ..
-				" -Dfile="       .. file				
+  assert(type(t.parameters)=="table")
+  for file, props in pairs(t.parameters) do
+    local repoinstall =
+      " -DgroupId="    .. props.groupId ..
+      " -DartifactId=" .. props.artifactId ..
+      " -Dversion="    .. props.version .. 
+      " -Dpackaging="  .. "jar" ..
+      " -Dfile="       .. file				
 				
-		if props.pomFile then
-			repoinstall = repoinstall .. " -DpomFile=" .. props.pomFile
-		end
+    if props.pomFile then
+      repoinstall = repoinstall .. " -DpomFile=" .. props.pomFile
+    end
 
-		build_cmd = "cd " .. build_dir .. " && " .. maven_cmd .. maven_args .. repoinstall
-		local ret = os.execute(build_cmd)
-		-- assert ensure that we could continue
-		assert(ret == 0,"ERROR compiling the software ".. t.name ..
-			" when it tried to install the file: ".. file .. 
-			" in the maven repository.")
-	end
+    build_cmd = "cd " .. build_dir .. " && " .. maven_cmd .. 
+      maven_args .. repoinstall
+    local ret = os.execute(build_cmd)
+    -- assert ensure that we could continue
+    assert(ret == 0,"ERROR compiling the software ".. t.name ..
+      " when it tried to install the file: ".. file .. 
+      " in the maven repository.")
+  end
 end
