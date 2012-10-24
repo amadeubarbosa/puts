1724e75ce3b18411357aa9cf74815da463b8c9d4
Amadeu Andrade Babosa Junior <amadeu@tecgraf.puc-rio.br>
2012-10-24 17:52:02 +0000

    [OPENBUS-1549] Refatorar o PUTS para uso fora do Core - alteração de nomes na hierarquia de pastas que o PUTS usa para evitar nomes   que remetam ao openbus
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/puts/trunk@134780 ae0415b3-e90b-0410-900d-d0be9363c56b

--- lua/tools/config.lua
+++ lua/tools/config.lua
@@ -1,11 +1,11 @@
 --- Default configuration used by package assistants.
--- Using the following directories structure.
+-- Using the following directories structure:
 -- <ul>
--- <li> openbus/lib/{a,b,c} = lua libs like lfs,luasocket,oil04,latt </li>
--- <li> openbus/lib/{d,e,f}     = base softwares like openldap,openssl </li>
--- <li> openbus/trunk          = svn export or checkout </li>
--- <li> openbus/install        = install dir to use install_files table to copy here like: <br />
--- <code> cp -r -Lf {a,b,c,d,e,f}/lib/$TEC_UNAME/* openbus/install/libpath/$TEC_UNAME/ </code></li>
+-- <li> puts/build/{a,b,c} = all source code of software being compiled </li>
+-- <li> puts/source        = (deprecated) main source code of openbus project (1.4.x and 1.5.x) </li>
+-- <li> puts/install       = directory similar to /usr of an unix machine, some
+--                           artifacts will be copied to here after compilation
+--                           (described on install_files table on descriptors) <br />
 -- </ul>
 -- The most important variables configured are documented as fields.
 -- @class table
@@ -15,9 +15,9 @@
 -- @field BASEDIR Base directory to place all stuff (external libs, source codes, metadatas, etc)
 -- @field PRODAPP <b>[Advanced]</b> Where the external packages will be extracted
 -- @field SVNURL <b>[Advanced]</b> Location of the source code to be compiled
--- @field SVNDIR <b>[Advanced]</b> Where the source code will be checked out
--- @field DEPLOYDIR <b>[Advanced]</b> Where are the 'puts' related configurations
--- @field INSTALL.TOP <b>[Advanced]</b> Where the binaries, libraries, includes and others will be place after the compilation
+-- @field SVNDIR <b>[Advanced]</b> (Depreacted) Where the source code will be checked out
+-- @field DEPLOYDIR <b>[Advanced]</b> (Deprecated) Where are the 'puts' related configurations
+-- @field INSTALL.TOP <b>[Advanced]</b> Where the binaries, libraries, includes and others will be stored after the compilation
 
 -- Tecmake compatibility variables
 -- You could (in a very special case) redefine this to work with the compile.lua
@@ -36,14 +36,14 @@ TEC_SYSNAME = TEC_SYSNAME or os.getenv("TEC_SYSNAME") or id.TEC_SYSNAME
 assert(TEC_SYSNAME, "ERROR: TEC_SYSNAME env var not defined")
 
 -- Base variables to compile and install time
-BASEDIR = BASEDIR or os.getenv("HOME") .."/openbus"
+BASEDIR = BASEDIR or os.getenv("HOME") .."/puts"
 assert(BASEDIR or os.getenv("HOME"), "ERROR: HOME env var not defined")
 
-PRODAPP = PRODAPP or BASEDIR .."/lib"
+PRODAPP = PRODAPP or BASEDIR .."/build"
 
 SVNREPURL = SVNREPURL or "svn+https://subversion.tecgraf.puc-rio.br/engdist"
 SVNURL = SVNURL or SVNREPURL .. "/openbus/trunk"
-SVNDIR = SVNDIR or BASEDIR .."/trunk"
+SVNDIR = SVNDIR or BASEDIR .."/source"
 DEPLOYDIR = DEPLOYDIR or SVNDIR .."/specs"
 DOWNLOADDIR = DOWNLOADDIR or BASEDIR .."/packs"
 PKGDIR = PKGDIR or DOWNLOADDIR .."/metadata/".. TEC_UNAME
