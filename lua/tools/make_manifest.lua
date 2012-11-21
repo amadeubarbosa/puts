74752b23b6467b214e6b0a151c8f3c5bb6417db5
Amadeu Andrade Babosa Junior <amadeu@tecgraf.puc-rio.br>
2012-11-21 21:37:39 +0000

    OPENBUS-1186 Incluir no Puts suporte a repositórios de descritores
    Melhora na mensagem de erro sob inexistencia do diretório
    
    
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/puts/trunk@135652 ae0415b3-e90b-0410-900d-d0be9363c56b

--- lua/tools/make_manifest.lua
+++ lua/tools/make_manifest.lua
@@ -1,18 +1,28 @@
 module("tools.make_manifest", package.seeall)
 
+local usage = function(cmd)
+  print(
+  "   Usage: ".. cmd .." <path>\n"..
+  "   <path>   : local path to the directory with *.desc files")
+  return false
+end
+
 function run()
   local path
   if not arg[1] then
-    print(
-  "   Usage: ".. arg[0] .." <path>\n"..
-  "   <path>   : local path to the directory with *.desc files")
-    return false
+    return usage(arg[0])
+  elseif (arg[1]:lower() == "help") or (arg[1]:lower() == "--help") then
+    return usage(arg[0])
   else
     path = arg[1]
   end
   manifest = require 'tools.manifest'
   util = require 'tools.util'
   local _, manif = manifest.rebuild_manifest(path)
+  if not manif or not manif.repository then
+    print("[ERROR  ] No such directory "..path)
+    return false
+  end
   for name, versions in pairs(manif.repository) do
     for version, metadatas in pairs(versions) do
       for _, meta in ipairs(metadatas) do
@@ -25,7 +35,7 @@ function run()
   end
   
   util.serialize_table(path.."/manifest", manif)
-  print("New manifest saved to "..path.."/manifest")
+  print("[INFO   ] Manifest saved to "..path.."/manifest")
   return true
 end
 
